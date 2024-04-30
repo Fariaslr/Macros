c4ea8194718bd8af522c38fbfc2ff08b7893f43d
Lucas Farias <83430368+Fariaslr@users.noreply.github.com>
2024-04-29 22:51:18 -0300

    :wrench: Relacionamento de classes

--- src/model/pessoa/Nutricionista.java
+++ src/model/pessoa/Nutricionista.java
@@ -1,15 +1,9 @@
 package model.pessoa;
 
-import java.util.List;
-import model.triagem.Consulta;
-import model.triagem.Plano;
-
 public class Nutricionista extends Pessoa {
 
     private String crn;
-    private List<Plano> planos;
-    private List<Consulta> consultas;
-
+    
     public String getCrn() {
         return crn;
     }
@@ -17,21 +11,5 @@ public class Nutricionista extends Pessoa {
     public void setCrn(String crn) {
         this.crn = crn;
     }    
-
-    public List<Plano> getPlanos() {
-        return planos;
-    }
-
-    public void setPlanos(List<Plano> planos) {
-        this.planos = planos;
-    }
-
-    public List<Consulta> getConsultas() {
-        return consultas;
-    }
-
-    public void setConsultas(List<Consulta> consultas) {
-        this.consultas = consultas;
-    }
     
 }
